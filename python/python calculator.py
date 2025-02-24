9cfeed0c6cdd877df7b48747dfd3e9a457daed07
unknown <QuanCorp@Starship1.Spacecorp.local>
2025-02-23 21:42:17 -0600

    Added new Python projects

--- python/python calculator.py
+++ python/python calculator.py
@@ -1,17 +1,23 @@
+while True:
+    # Get operands from user
+    num1 = int(input("Enter first number: "))
+    op = input("Enter operator (+, -, *, /): ")
+    num2 = int(input("Enter second number: "))
 
-# Get operands from user
-num1 = int(input("Enter first number: "))
-op = input("Enter operator (+, -, * /): ")
-num2 = int(input("Enter second number: "))
+    # Perform calculation
+    if op == "+":
+        print(num1 + num2)
+    elif op == "-":
+        print(num1 - num2)
+    elif op == "*":
+        print(num1 * num2)
+    elif op == "/":
+        print(num1 / num2)
+    else:
+        print("That is not a recognized operator")
 
-# Perform calculation
-if op == "+":
-    print(num1 + num2)
-elif op == "-":
-    print(num1 - num2)
-elif op == "*":
-    print(num1 * num2)
-elif op == "/":
-    print(num1 / num2)
-else:
-    print("Invalid operator")
+    # Ask if user wants to perform another calculation
+    again = input("Do you want to perform another calculation? (yes/no): ").lower()
+    if again != 'yes':
+        print("Goodbye!")
+        break
