286991279a72333d0831de83e88fd5ab85286d0c
unknown <QuanCorp@Starship1.Spacecorp.local>
2025-02-27 20:53:57 -0600

    Added new Python projects

--- python/python calculator.py
+++ python/python calculator.py
@@ -1,23 +1,27 @@
 while True:
-    # Get operands from user
-    num1 = int(input("Enter first number: "))
-    op = input("Enter operator (+, -, *, /): ")
-    num2 = int(input("Enter second number: "))
+    try:
+        num1 = float(input("Enter first number: "))
+        op = input("Enter operator (+, -, *, /): ")
+        num2 = float(input("Enter second number: "))
 
-    # Perform calculation
-    if op == "+":
-        print(num1 + num2)
-    elif op == "-":
-        print(num1 - num2)
-    elif op == "*":
-        print(num1 * num2)
-    elif op == "/":
-        print(num1 / num2)
-    else:
-        print("That is not a recognized operator")
+        if op == "+":
+            print(f"{num1} + {num2} = {num1 + num2}")
+        elif op == "-":
+            print(f"{num1} - {num2} = {num1 - num2}")
+        elif op == "*":
+            print(f"{num1} * {num2} = {num1 * num2}")
+        elif op == "/":
+            if num2 != 0:
+                print(f"{num1} / {num2} = {num1 / num2}")
+            else:
+                print("Division by zero is undefined.")
+        else:
+            print("That is not a recognized operator.")
 
-    # Ask if user wants to perform another calculation
+    except ValueError:
+        print("Invalid input. Please enter numerical values.")
+
     again = input("Do you want to perform another calculation? (yes/no): ").lower()
     if again != 'yes':
-        print("Goodbye!")
+        print("Peace!")
         break
